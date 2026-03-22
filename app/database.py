cb4b96c8823850bee2869e4eed9dbdf17fbadb77
Erick Fritsche <frozenerick1@gmail.com>
2026-03-22 17:35:14 -0400

    Fix database SessionLocal and get_db function

--- app/database.py
+++ app/database.py
@@ -4,7 +4,14 @@ from ..config import Config
 
 
 engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
-session_local = sessionmaker(auto_commit=False, autoFlush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+
+def get_db():
+    db = SessionLocal() 
+    try:
+        yield db 
+    finally: 
+        db.close()
 
